e8f69ffb6d61608f1e5f17ff02c5089cd473fefe
张伟峰 <zhang.weifeng@tslsmart.com>
2022-07-26 18:43:56 +0800

    feat: 增加函数中的可选参数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,3 +31,52 @@ const longerArray = longest([1, 2], [1, 2, 3]);
 const longerString = longest("alice", "bob");
 // Error! Numbers don't have a 'length' property
 const notOK = longest(10, 100);
+
+// function minimumLength<Type extends { length: number }>(obj: Type, minimum: number): Type {
+//   if (obj.length > minimum) {
+//     return obj;
+//   } else {
+//     return {
+//       length: minimum,
+//     };
+//   }
+// }
+
+function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+  return arr1.concat(arr2);
+}
+// 传递类型参数
+const arr = combine<string | number | boolean>([1, 2, 3], ["hello"]);
+
+// 函数中的可选参数
+declare function f(x?: number): void;
+f(10);
+
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i);
+  }
+}
+
+myForEach([1, 2, 3], (a, i) => console.log(i.toFixed()));
+myForEach([1, 2, 3], (a) => console.log(a));
+
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+const d3 = makeDate(1, 3);
+
+function fn(x: string | boolean): void;
+function fn() {
+  // ...
+}
+fn();
